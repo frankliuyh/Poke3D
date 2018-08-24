7df62c4cf237a126c7c28f998b92328d8a699aed
frankliuyh <frankliuyh@qq.com>
2018-08-24 22:19:56 +0800

    add another Pokemon model

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -64,11 +64,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             node.addChildNode(planeNode)
             
-            if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
-                if let pokeNode = pokeScene.rootNode.childNodes.first {
-                    pokeNode.eulerAngles.x = .pi / 2
-                    
-                    planeNode.addChildNode(pokeNode)
+            if let name = imageAnchor.referenceImage.name {
+                if let pokeScene = SCNScene(named: "art.scnassets/\(name)" + "/\(name).scn") {
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = .pi / 2
+                        
+                        planeNode.addChildNode(pokeNode)
+                    }
                 }
             }
         }
